d176ea7ee1311b06d73371d177307c072e1708be
Nandhan Kumar Akula <104351901+nandhankumar@users.noreply.github.com>
2025-07-19 16:50:17 +0530

    Update main.tf

--- web/main.tf
+++ web/main.tf
@@ -1,7 +1,7 @@
 #ec2
 
 resource "aws_instance" "server" {
-  ami = "ami-0440d3b780d96b29d"
+  ami = "ami-0150ccaf51ab55a51"
   instance_type = "t2.micro"
   subnet_id = var.sn
   security_groups = [var.sg]
